dfd07250cc689416038a5319a80242edad755b11
Alexey Kopytko <alexey@kopytko.com>
2020-09-20 23:54:53 +0900

    Fix CustomTagTest

--- tests/Liquid/CustomTagTest.php
+++ tests/Liquid/CustomTagTest.php
@@ -19,21 +19,28 @@ class TagFoo extends TagComment
 
 class CustomTagTest extends TestCase
 {
-	/**
-	 */
 	public function testUnknownTag()
 	{
+		$template = new Template();
+
+		if (array_key_exists('foo', $template->getTags())) {
+			$this->markTestIncomplete("Test tag already registered. Are you missing @depends?");
+		}
+
 		$this->expectException(\Liquid\Exception\ParseException::class);
 		$this->expectExceptionMessage('Unknown tag foo');
 
-		$template = new Template();
 		$template->parse('[ba{% foo %} Comment {% endfoo %}r]');
 	}
 
+	/**
+	 * @depends testUnknownTag
+	 */
 	public function testCustomTag()
 	{
 		$template = new Template();
 		$template->registerTag('foo', TagFoo::class);
+
 		$template->parse('[ba{% foo %} Comment {% endfoo %}r]');
 		$this->assertEquals('[bar]', $template->render());
 	}
